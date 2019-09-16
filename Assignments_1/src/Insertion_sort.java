235adb137739ecd4672c9957311786b84813df29
lsh23 <shseoul14@gmail.com>
2019-09-16 19:17:48 +0900

    Add sort function
    삽입정렬 로직에 맞게 코드 작성 및
    테스트 코드 추가.

--- Assignments_1/src/Insertion_sort.java
+++ Assignments_1/src/Insertion_sort.java
@@ -1,6 +1,32 @@
+import java.util.Arrays;
+
 public class Insertion_sort {
+
     public static <T extends Number> T[] sort (T[] array){
         //Insertion sort;
+        for ( int i = 1; i < array.length ; i++){
+            T key = array[i];
+            for(int j = i-1 ; j >=0 ; j--){
+                while( j >= 0 && array[j].doubleValue() > key.doubleValue()) {
+                    T tmp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tmp;
+                    j = j - 1;
+                }
+                array[j+1]=key;
+                break;
+            }
+        }
         return array;
+    }
+
+    public static void main(String[] args) {
+//        Integer[] integers = { 1, 34, 6, 5, 2};
+//        Integer[] integers = { 1, 2, 6, 5, 34};
+        Integer[] integers = { 34, 1, 6, 5, 2};
+//        Double[] doubles = { 1.0, 2.0,3.0 , 54.0, 34.0};
+        Integer[] sorted = Insertion_sort.sort(integers);
+        System.out.println(Arrays.toString(sorted));
+
     }
 }
