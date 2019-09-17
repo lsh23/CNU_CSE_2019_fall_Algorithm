057b57ed8f160285abd3dd9e233343c7c81a0124
lsh23 <shseoul14@gmail.com>
2019-09-17 16:29:00 +0900

    Add fibonacci_recursive function code
    재귀방법 코드 추가

--- Assignments_2/src/Fibonacci.java
+++ Assignments_2/src/Fibonacci.java
@@ -1,7 +1,9 @@
 public class Fibonacci {
 
     public static int fibonacci_recursive(int n){
-        return 0;
+        if(n == 0) return 1;
+        if(n == 1) return 1;
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2);
     }
 
     public static int fibonacci_bottom_up(int n){
