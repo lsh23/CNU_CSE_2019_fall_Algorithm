b95a011c5ae0a76d4355d0b65ce769b4c12a8884
lsh23 <shseoul14@gmail.com>
2019-09-17 16:27:07 +0900

    Upload Fibonacci.java 와 test.java
    Upload Fibonacci.java 와 test.java
    스켈레톤 코드 업로드

---
+++ Assignments_2/src/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+
+    public static int fibonacci_recursive(int n){
+        return 0;
+    }
+
+    public static int fibonacci_bottom_up(int n){
+        return 0;
+    }
+
+    public static int fibonacci_recursive_squaring(int n){
+        return 0;
+    }
+}
---
+++ Assignments_2/src/Fibonacci_test.java
@@ -0,0 +1,7 @@
+public class Fibonacci_test {
+    public static void main(String[] args) {
+        System.out.println(Fibonacci.fibonacci_recursive(7));
+        System.out.println(Fibonacci.fibonacci_bottom_up(7));
+        System.out.println(Fibonacci.fibonacci_recursive_squaring(7));
+    }
+}
