cdb1d28de2041cab8b5be0f215818cbc294f6a95
lsh23 <shseoul14@gmail.com>
2019-10-04 14:29:30 +0900

    upload skeleton code
    스켈레톤 코드 업로드

---
+++ Assignments_4/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            //파일 객체 생성
+            File file = new File("data05.txt");
+            //입력 스트림 생성
+            FileReader filereader = new FileReader(file);
+            //입력 버퍼 생성
+            BufferedReader bufReader = new BufferedReader(filereader);
+            String line = "";
+            while((line = bufReader.readLine()) != null){
+                System.out.println(line);
+            }
+            bufReader.close();
+        }catch (FileNotFoundException e) {
+            // TODO: handle exception
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+}
