d23b0df3ca641e7fde3bbbaafa12e06d53d21a16
lsh23 <shseoul14@gmail.com>
2019-10-04 15:45:49 +0900

    add Heapify function
    max Heapfify 함수 추가

--- Assignments_4/src/Heap.java
+++ Assignments_4/src/Heap.java
@@ -8,6 +8,33 @@ public class Heap {
         this.array_tree = array;
     }
 
+    void bulid_max_heap(){
+        for (int i = heap_size/2 ; i>=1 ;i--){
+            max_heapity(i);
+        }
+    }
+
+    void max_heapity(int i){
+        int l = get_left_child_index(i);
+        int r = get_rigth_child_index(i);
+        if(l>heap_size) return;
+        int max = 0;
+        if(l<=heap_size && array_tree[l].get_key()>array_tree[i].get_key()){
+            max = l;
+        }
+        else max = i;
+
+        if(r<=heap_size && array_tree[r].get_key()>array_tree[max].get_key()){
+            max = r;
+        }
+        if (max!=i){
+            Pair tmp = array_tree[i];
+            array_tree[i] = array_tree[max];
+            array_tree[max] = tmp;
+            max_heapity(max);
+        }
+    }
+
     
     int get_parent_index(int i){
         return i/2;
