383f83992af17356245ca5ee0bc1970e62003cc8
lsh23 <shseoul14@gmail.com>
2019-10-04 15:44:10 +0900

    add Heap generator code
    생성자 코드 추가

--- Assignments_4/src/Heap.java
+++ Assignments_4/src/Heap.java
@@ -1,2 +1,12 @@
+import java.util.HashMap;
+
 public class Heap {
+    Pair[] array_tree;
+    int heap_size;
+    Heap(Pair[] array,int size){
+        this.heap_size = size;
+        this.array_tree = array;
+    }
+
+
 }
