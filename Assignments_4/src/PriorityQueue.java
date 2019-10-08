b3bff985da03c601931f94c8299da101aaecd7c1
lsh23 <shseoul14@gmail.com>
2019-10-08 22:38:16 +0900

    add PriorityQueue Method
    기존 max_heap의 메소드를 거의 그대로 사용하도록 함
    구조가 좋지 않은데 과제 제출 후 정리 다시 할것...

--- Assignments_4/src/PriorityQueue.java
+++ Assignments_4/src/PriorityQueue.java
@@ -1,2 +1,32 @@
 public class PriorityQueue {
+    Heap max_heap;
+
+    PriorityQueue(Heap heap){
+        this.max_heap = heap;
+    }
+
+    void insert(Pair pair){
+        max_heap.insert(pair);
+    }
+
+    Pair max(){
+        return max_heap.get_max();
+    }
+
+    Pair extract_max(){
+        return max_heap.extract_max();
+    }
+
+    void increase_key(int key, int k){
+        max_heap.increase_key(key,k);
+    }
+
+    boolean delete(int key){
+        return max_heap.delete(key);
+    }
+
+    @Override
+    public String toString() {
+        return max_heap.toString();
+    }
 }
