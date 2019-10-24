d49cb799d7a510d3e0df78214e5f308fb550f141
lsh23 <shseoul14@gmail.com>
2019-10-24 22:11:43 +0900

    Add binarySearch code
    Loop 형태의 binarySearch 구현
    array와 array에서 찾을 n을 입력 받는다.

--- Assignments_6/src/BinarySearch.java
+++ Assignments_6/src/BinarySearch.java
@@ -1,3 +1,20 @@
 public class BinarySearch {
+    public static int binarySearch(int[] array, int n) {
 
+        int end = array.length - 1;
+        int start = 0;
+        int middle;
+
+        while (end > start) {
+            middle = (start + end) / 2;
+            if (array[middle] > n) {
+                end = middle;
+            } else if (array[middle] < n) {
+                start = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
 }
